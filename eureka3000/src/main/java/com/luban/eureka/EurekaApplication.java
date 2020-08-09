75665169af6c185e3e3702644fe862229e680844
meet-liuyongkang <1660671346@qq.com>
2020-08-09 20:34:45 +0800

    eureka

--- eureka3000/src/main/java/com/luban/eureka/EurekaApplication.java
+++ eureka3000/src/main/java/com/luban/eureka/EurekaApplication.java
@@ -2,6 +2,7 @@ package com.luban.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author <a href="mailto:jiangyue@dtstack.com">江月 At 袋鼠云</a>.
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/8/9 11:37 上午
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
--- user/src/main/java/com/luban/user/UserApplication.java
+++ user/src/main/java/com/luban/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.luban.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author <a href="mailto:jiangyue@dtstack.com">江月 At 袋鼠云</a>.
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/8/7 9:43 上午
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UserApplication {
 
     public static void main(String[] args) {
